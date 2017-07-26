8744d420263b5ed9549e58631c9467d625b53a01
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-26 13:04:41 -0700

    adds additional orm and burger.js code

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,21 @@
 var connection = require('./connection.js')
 
 var orm = {
-    newSandwich: function(burger) {
-        connection.query()
+    newSandwich: function(table, col, burger, cb) {
+        var column = col.toString()
+        connection.query("INSERT INTO ??( ? ) VALUES( ? )", [burger], function(err, res) {
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    eatSandwich: function(table, burgerId, eaten, cb) {
+        connection.query("UPDATE ?? SET devored = ? WHERE id = ?", [table, eaten, burgerId], function(err, res) {
+            if (err) throw err;
+            cb(res)
+        });
+    },
+    all: function(table, cb){
+        connection.query("SELECT ALL from")
     }
 }
 
