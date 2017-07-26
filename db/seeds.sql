3c1eecbf896e9b2406982503bd1bc0c41714dbf0
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-26 09:30:29 -0700

    adds database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (burger_name, date) VALUES()
+INSERT INTO burgers (burger_name) VALUES("Turkey with Avocado");
+INSERT INTO burgers (burger_name) VALUES("Double Beef with Bacon");
+INSERT INTO burgers (burger_name) VALUES("Chicken Sandwich with Swiss");
